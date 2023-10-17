d30562a909fd3437d007908ee13e53fedf232e8e
Enrique de Ridder <enriquederidder5@gmail.com>
2023-10-17 20:13:48 +0200

    ejer rotar

--- app/src/main/java/com/example/practica1_pmdm/MainActivity.kt
+++ app/src/main/java/com/example/practica1_pmdm/MainActivity.kt
@@ -5,9 +5,12 @@ import android.os.Bundle
 import android.util.Log
 
 class MainActivity : AppCompatActivity() {
+
+    var EDR:Int=0;
     var contador:Int=0;
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        EDR = 0;
         contador++;
         Log.i("ESTADOS","Creando la actividad:"+contador)
         Log.i("ESTADOS","Evento onCreate:"+contador)
@@ -16,20 +19,24 @@ class MainActivity : AppCompatActivity() {
     override fun onStart() {
         super.onStart()
         contador++;
+        EDR++;
         Log.i("ESTADOS","Evento onStart:"+contador)
     }
     override fun onResume() {
         super.onResume()
         contador++;
+        EDR++;
         Log.i("ESTADOS","Evento onResume:"+contador)
     }
     override fun onStop() {
         super.onStop()
         contador++;
+        EDR++;
         Log.i("ESTADOS","Evento onStop:"+contador)
     }
     override fun onDestroy() {
         super.onDestroy()
+        EDR++;
         Log.i("ESTADOS","Evento onDestroy:"+contador)
     }
 }
