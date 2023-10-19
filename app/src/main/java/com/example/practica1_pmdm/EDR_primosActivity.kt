bba9ee305ff7c7535c2c0a69790dd66382625157
enriq <enriquederidder5@gmail.com>
2023-10-19 18:13:46 +0200

    primo

--- app/src/main/java/com/example/practica1_pmdm/EDR_primosActivity.kt
+++ app/src/main/java/com/example/practica1_pmdm/EDR_primosActivity.kt
@@ -1,11 +1,43 @@
 package com.example.practica1_pmdm
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class EDR_primosActivity : AppCompatActivity() {
+
+    lateinit var botonVolver: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_edr_primos)
+
+        botonVolver = findViewById(R.id.buttonReturn)
+        botonVolver.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
+
+    }
+    fun cal_primos(n:Int):ArrayList<Int>{
+        var elementos = ArrayList<Int>()
+
+        for(i in 1  ..  n){
+            if(esPrimo(i, i-1)){
+                elementos.add(i)
+            }
+        }
+
+        return elementos
+    }
+    private fun esPrimo(n: Int, divisor: Int): Boolean {
+        if (divisor <= 1) {
+            return true
+        } else if (n % divisor == 0) {
+            return false
+        } else {
+            return esPrimo(n, divisor - 1)
+        }
     }
 }
--- app/src/main/java/com/example/practica1_pmdm/MainActivity.kt
+++ app/src/main/java/com/example/practica1_pmdm/MainActivity.kt
@@ -1,13 +1,18 @@
 package com.example.practica1_pmdm
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
 
     var EDR:Int=0;
     var contador:Int=0;
+    lateinit var buttonReturn: Button
+
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         EDR = 0;
@@ -15,6 +20,17 @@ class MainActivity : AppCompatActivity() {
         Log.i("ESTADOS","Creando la actividad:"+contador)
         Log.i("ESTADOS","Evento onCreate:"+contador)
         setContentView(R.layout.activity_main)
+
+        buttonReturn = findViewById(R.id.buttonReturn)
+
+        buttonReturn.setOnClickListener {
+            val numPrimoActivity = EDR_primosActivity()
+            val numero = 9
+            val resultados = numPrimoActivity.cal_primos(numero)
+
+            Log.d("MainActivity", "Números primos: $resultados")
+        }
+
     }
     override fun onStart() {
         super.onStart()
